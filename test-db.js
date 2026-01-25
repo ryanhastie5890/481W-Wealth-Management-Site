2df32bc999f399397fffeedf776c26cd753631a5
Ryan Hastie <231561729+ryanhastie5890@users.noreply.github.com>
2026-01-25 12:11:29 -0500

    Delete test-db.js

--- test-db.js
+++
@@ -1,9 +0,0 @@
-const dbCon = require('./database.js').dbCon;
-
-dbCon.query('SELECT 1 + 1 AS result', function (error, results) {
-  if (error) {
-    console.log("Query error:", error);
-    return;
-  }
-  console.log("Database responded. Result =", results[0].result);
-});
