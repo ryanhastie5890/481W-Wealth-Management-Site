1308c6edaaaeab73b042e123ae2825d7bac1ad93
rbutle16 <rbutle16@emich.edu>
2026-02-12 12:15:15 -0500

    User auth required to access anything besides login/helper endpoints

---
+++ src/services/auth.js
@@ -0,0 +1,18 @@
+export default function requireAuth(req, res, next) {
+    if (req.session.userId) {
+        return next();
+    }
+
+    // Allow auth + public assets
+    if (
+        req.path === '/login.html' ||
+        req.path.startsWith('/css/') ||
+        req.path.startsWith('/js/') ||
+        req.path.startsWith('/api/auth/') ||
+        req.path.startsWith('/api/session/')
+    ) {
+        return next();
+    }
+
+    return res.redirect('/login.html');
+}
